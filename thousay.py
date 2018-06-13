f0fdeff26968468f85bfcfd1d265e75f3ba58a22
No0to <leocdean@gmail.com>
2018-06-12 17:19:24 -0700

    'thousay.py'

---
+++ thousay.py
@@ -0,0 +1,20 @@
+from colorama import *
+import os
+
+os.system('cls') # Clears the screen
+
+print(Fore.RED + 'Made by Thou')
+print(Fore.RED + '@Th0u__')
+
+print() # Blank line
+
+print(Fore.WHITE + 'What would you like the cow to say?')
+
+mainInput = input(":") # The thing that shows ':'
+
+print("  < ", mainInput, " >")
+print("        \   ^__^")
+print("         \  (oo)\_______")
+print("            (__)\       )\/")
+print("                ||----w |")
+print("                ||     ||")
